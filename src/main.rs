e3049e1a13a70e6360c3c407cac75266db5d34c3
Maxence Ahlouche <maxence.ahlouche@gmail.com>
2023-06-26 19:43:54 +0200

    Implement and test the core greeting function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+// Send a greeting
+fn say_hello(name: Option<&str>) -> String {
+    // if a name was not provided, address the greeting to "stranger"
+    let name = name.unwrap_or("stranger");
+
+    format!("Hello, {name}!")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let response = say_hello(Some("world"));
+    println!("{}", response);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_name_provided() {
+        let name = "world";
+        let result = say_hello(Some(name));
+        assert_eq!(format!("Hello, {name}!"), result);
+    }
+
+    #[test]
+    fn test_no_name_provided() {
+        let result = say_hello(None);
+        assert_eq!("Hello, stranger!".to_owned(), result);
+    }
 }
