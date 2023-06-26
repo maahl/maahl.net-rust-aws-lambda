a2c68c92507c254b25dca19ae67960f9009e4623
Maxence Ahlouche <maxence.ahlouche@gmail.com>
2023-06-26 22:08:15 +0200

    Interact with the Lambda runtime

--- readme.md
+++ readme.md
@@ -4,3 +4,8 @@ This repository is a companion to my [blog article](https://maahl.net/blog/rust-
 Each commit corresponds to a section in that article, so you can easily follow along.
 
 Tested with Rust 1.73.0, Terraform 1.6.3.
+
+## Resources
+
+* [The Rust Lambda runtime](https://github.com/awslabs/aws-lambda-rust-runtime)
+* [Cargo Lambda](https://www.cargo-lambda.info/)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use lambda_runtime::{service_fn, Error, LambdaEvent};
+use serde_json::{json, Value};
+
 // Send a greeting
 fn say_hello(name: Option<&str>) -> String {
     // if a name was not provided, address the greeting to "stranger"
@@ -6,14 +9,27 @@ fn say_hello(name: Option<&str>) -> String {
     format!("Hello, {name}!")
 }
 
-fn main() {
-    let response = say_hello(Some("world"));
-    println!("{}", response);
+// Wrapper for our core function
+// Its role is to extract the relevant info from the incoming event, and convert the
+// response to json.
+async fn run_lambda(event: LambdaEvent<Value>) -> Result<Value, Error> {
+    let (event, _context) = event.into_parts();
+
+    let name = event["name"].as_str();
+    let result = say_hello(name);
+
+    Ok(json!(result))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda_runtime::run(service_fn(run_lambda)).await
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use lambda_runtime::{Context, LambdaEvent};
 
     #[test]
     fn test_name_provided() {
@@ -26,5 +42,33 @@ mod tests {
     fn test_no_name_provided() {
         let result = say_hello(None);
         assert_eq!("Hello, stranger!".to_owned(), result);
+    }
+
+    #[tokio::test]
+    async fn test_wrapper_name_provided() {
+        let name = "world";
+        let event = LambdaEvent::new(json!({ "name": name }), Context::default());
+        let expected_result = json!(format!("Hello, {name}!"));
+
+        let result = run_lambda(event).await;
+
+        assert!(result.is_ok());
+        let result = result.unwrap();
+        assert_eq!(result, expected_result);
+    }
+
+    #[tokio::test]
+    async fn test_wrapper_no_name_provided() {
+        let event = LambdaEvent::new(
+            json!({ "meaningless_key": "meaningless_value" }),
+            Context::default(),
+        );
+        let expected_result = json!(format!("Hello, stranger!"));
+
+        let result = run_lambda(event).await;
+
+        assert!(result.is_ok());
+        let result = result.unwrap();
+        assert_eq!(result, expected_result);
     }
 }
